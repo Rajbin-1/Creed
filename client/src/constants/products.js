4c002312b8187d0dcb154afe6eab207d3a83a77f
Rajbin-1 <221181143+Rajbin-1@users.noreply.github.com>
2026-03-27 22:15:45 -0400

    Add: Dynamic product architecture with stock.json and products.js constants

---
+++ client/src/constants/products.js
@@ -0,0 +1,153 @@
+/**
+ * Creed Lifestyle Nepal - Product Catalog
+ * All products linked to official Daraz store
+ * Stock status managed via /public/stock.json
+ */
+
+export const PRODUCTS = [
+  {
+    id: 'hair-volumizing-powder',
+    name: 'Hair Volumizing Powder',
+    category: 'Hair Care',
+    description: 'Premium hair volumizing powder for instant volume and texture',
+    price: 1299,
+    darazLink: 'https://s.daraz.com.np/s.Z00Jl',
+    image: 'https://d2xsxph8kpxj0f.cloudfront.net/310519663483354275/bXM8D6oMMGwALEvguBMTpw/hair-volumizing-powder.jpg',
+    feature: 'Hair Volume',
+    icon: 'Zap',
+    isCombo: false,
+    highlight: false,
+  },
+  {
+    id: 'ball-body-trimmer',
+    name: 'Ball and Body Trimmer',
+    category: 'Grooming Tools',
+    description: 'Waterproof body trimmer with precision blades',
+    price: 2499,
+    darazLink: 'https://s.daraz.com.np/s.Z00Jr',
+    image: 'https://d2xsxph8kpxj0f.cloudfront.net/310519663483354275/bXM8D6oMMGwALEvguBMTpw/ball-body-trimmer.jpg',
+    feature: 'Precision Trim',
+    icon: 'Scissors',
+    isCombo: false,
+    highlight: false,
+  },
+  {
+    id: 'beard-groomer-pro',
+    name: 'Beard Groomer Pro',
+    category: 'Beard Care',
+    description: 'Professional beard grooming tool with adjustable settings',
+    price: 1899,
+    darazLink: 'https://s.daraz.com.np/s.Z00JC',
+    image: 'https://d2xsxph8kpxj0f.cloudfront.net/310519663483354275/bXM8D6oMMGwALEvguBMTpw/beard-groomer-pro.jpg',
+    feature: 'Beard Styling',
+    icon: 'Zap',
+    isCombo: false,
+    highlight: false,
+  },
+  {
+    id: 'silicone-scalp-massager',
+    name: 'Silicone Scalp Massager',
+    category: 'Scalp Care',
+    description: 'Electric scalp massager for improved blood circulation',
+    price: 1599,
+    darazLink: 'https://s.daraz.com.np/s.Z00rY',
+    image: 'https://d2xsxph8kpxj0f.cloudfront.net/310519663483354275/bXM8D6oMMGwALEvguBMTpw/silicone-scalp-massager.jpg',
+    feature: 'Scalp Health',
+    icon: 'Zap',
+    isCombo: false,
+    highlight: false,
+  },
+  {
+    id: 'exfoliator',
+    name: 'Exfoliator',
+    category: 'Skin Care',
+    description: 'Premium body exfoliator for smooth, healthy skin',
+    price: 899,
+    darazLink: 'https://s.daraz.com.np/s.Z00rf',
+    image: 'https://d2xsxph8kpxj0f.cloudfront.net/310519663483354275/bXM8D6oMMGwALEvguBMTpw/exfoliator.jpg',
+    feature: 'Skin Polish',
+    icon: 'Sparkles',
+    isCombo: false,
+    highlight: false,
+  },
+  {
+    id: 'back-exfoliator',
+    name: 'Back Exfoliator',
+    category: 'Skin Care',
+    description: 'Specialized back exfoliator for hard-to-reach areas',
+    price: 999,
+    darazLink: 'https://s.daraz.com.np/s.Z00Ia',
+    image: 'https://d2xsxph8kpxj0f.cloudfront.net/310519663483354275/bXM8D6oMMGwALEvguBMTpw/back-exfoliator.jpg',
+    feature: 'Back Care',
+    icon: 'Sparkles',
+    isCombo: false,
+    highlight: false,
+  },
+  {
+    id: 'power-trio',
+    name: 'Power Trio',
+    category: 'Combo Packages',
+    description: 'Ultimate combo: Scalp Massager + Back & Body Exfoliator + Bonus Items',
+    price: 4299,
+    darazLink: 'https://s.daraz.com.np/s.Z00I4',
+    image: 'https://d2xsxph8kpxj0f.cloudfront.net/310519663483354275/bXM8D6oMMGwALEvguBMTpw/power-trio-combo.jpg',
+    feature: 'Elite Combo',
+    icon: 'Crown',
+    isCombo: true,
+    highlight: true,
+    badge: 'Best Value',
+    shimmer: true,
+    items: ['Silicone Scalp Massager', 'Back Exfoliator', 'Body Exfoliator'],
+  },
+  {
+    id: 'tactical-grooming-duo',
+    name: 'Tactical Grooming Duo',
+    category: 'Combo Packages',
+    description: 'Premium duo: Scalp Massager + Body Exfoliator for complete grooming',
+    price: 3299,
+    darazLink: 'https://s.daraz.com.np/s.Z00rN',
+    image: 'https://d2xsxph8kpxj0f.cloudfront.net/310519663483354275/bXM8D6oMMGwALEvguBMTpw/tactical-duo-combo.jpg',
+    feature: 'Elite Combo',
+    icon: 'Crown',
+    isCombo: true,
+    highlight: true,
+    badge: 'Best Value',
+    shimmer: true,
+    items: ['Silicone Scalp Massager', 'Body Exfoliator'],
+  },
+];
+
+export const SOCIAL_LINKS = {
+  instagram: {
+    url: 'https://www.instagram.com/thecreedlifestyle?igsh=MWs1cmJiM21rMHNtaw==',
+    label: 'Instagram',
+    icon: 'Instagram',
+  },
+  facebook: {
+    url: 'https://www.facebook.com/share/1FHbzcrj9v/',
+    label: 'Facebook',
+    icon: 'Facebook',
+  },
+  daraz: {
+    url: 'https://s.daraz.com.np/s.Z00rj',
+    label: 'Daraz Store',
+    icon: 'ShoppingBag',
+  },
+  tiktok: {
+    url: 'https://www.tiktok.com/@creed.lifestyle',
+    label: 'TikTok',
+    icon: 'Music',
+  },
+};
+
+export const getProductById = (id) => {
+  return PRODUCTS.find((product) => product.id === id);
+};
+
+export const getComboProducts = () => {
+  return PRODUCTS.filter((product) => product.isCombo);
+};
+
+export const getStandardProducts = () => {
+  return PRODUCTS.filter((product) => !product.isCombo);
+};
